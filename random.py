adf00bdb73811fd2559eb1e53e7a916abcb95068
Neil Maheshwari <Neil@Neils-MacBook-Pro.local>
2016-06-12 22:48:32 -0700

    Adds stats for network, interrupts, and ctx switches

--- random.py
+++ random.py
@@ -13,7 +13,7 @@ def cpu_user_time():
 # Returns the system-wide CPU utilization as a percentage for each
 # CPU. Blocks for i seconds. 
 def cpu_utilization_percentages(i):
-    percents = psutil.cpu_times_percent(interval = i, percpu = True)
+    percents = psutil.cpu_times_percent(interval = i)
     return percents
 
 # Returns the system-wide number of reads
@@ -26,6 +26,26 @@ def write_count():
     count = psutil.disk_io_counters().write_count
     return count
 
+# Returns system wide number of bytes sent
+def bytes_sent():
+    sent = psutil.net_io_counters().bytes_sent
+    return sent
+
+# Returns system wide number of bytes received
+def bytes_recv():
+    recv = psutil.net_io_counters().bytes_recv
+    return recv
+
+# Returns number of interrupts since boot
+def interrupts_count():
+    count = psutil.cpu_stats().interrupts
+    return count
+
+# Returns the number of context switches since boot
+def ctx_switches():
+    count = psutil.cpu_stats().ctx_switches
+    return count
+
 if __name__ == '__main__':
 
     if len(sys.argv) == 1:
@@ -35,5 +55,9 @@ if __name__ == '__main__':
         print("CPU utilization: {}".format(cpu_utilization_percentages(0.1)))
         print("Read count: {}".format(read_count()))
         print("Write count: {}".format(write_count()))
+        print("Bytes sent: {}".format(bytes_sent()))
+        print("Bytes received: {}".format(bytes_recv()))
+        print("Interrupts count: {}".format(interrupts_count()))
+        print("Context switches: {}".format(ctx_switches()))
     else:
         print("Unexpected input")        
